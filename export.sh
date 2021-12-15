1e208c80e162ae98b975e1fa09be0b73915a0112
Ilovemyhouse <50217071+Ilovemyhous@users.noreply.github.com>
2021-12-15 19:31:03 +0100

    Some edits by @Amiralgaby.
    Some changes, like version a version variable, along with changes with the variables with the timer itself.
    Huge thanks to @Amiralgaby for the help!

--- export.sh
+++ export.sh
@@ -1,3 +1,10 @@
-export PATH=/opt/devkitpro/devkitPPC/lib/gcc/powerpc-eabi/11.1.0:$PATH
-export DEVKITPPC=/opt/devkitPPC
+bash # Fish is bugged for some reason.
+# At Terminal startup.
+echo "export DEVKITPPC=/opt/devkitpro/devkitPPC" >> ~/.bashrc
+echo "export DEVKITARM=/opt/devkitpro/devkitARM" >> ~/.bashrc
+echo "export DEVKITPRO=/opt/devkitpro" >> ~/.bashrc
+# On the fly.
+export DEVKITPPC=/opt/devkitpro/devkitPPC
+export DEVKITARM=/opt/devkitpro/devkitARM
 export DEVKITPRO=/opt/devkitpro
+source ~/.bashrc
